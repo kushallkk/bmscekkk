87ed8e61382553d97f900b0433690cb0ad68939a
kushallkk <kushallk.cs23@bmsce.ac.in>
2024-10-14 02:01:28 +0530

    Update quadratic.java

--- quadratic.java
+++ quadratic.java
@@ -1,32 +1,44 @@
-import java.util.scanner;
+import java.util.Scanner;
 import java.lang.Math;
-class quadratic{
-public static void main(string arg[]){
-System.out.println("enter the value of a");
-Scanner sc = new Scanner(System.in);
-int a = sc.nextInt();
-System.out.println("enter the value of b");
-int b = sc.nextInt();
-System.out.println("enter the value of a");
-int c = sc.nextInt();
-if(a==0){
-System.out.println("there is no quadratic equation");
-int d = b*b-4*a*c;
-if(d==0){
-int r1=((-b)/(2*a));
-System.out.println("the roots are real and equal:"+r1);
-}
-else if(d>0){
-int r1=((-b)+(Math.sqrt(d)))/(double)(2*a);
-int r2 = ((-b)-(Math.sqrt(d)))/(double)(2*a);
-System.out.println("the roots are:"+r1+" and "+r2);
-}
-else if (d<0){
-System.out.println("the roots are imaginary");
-int r1 = (-b)/(2*a);
-int r2 = Math.sqrt(-d)/(2*a);
 
-}
-}
+class Quadratic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter the value of a:");
+        int a = sc.nextInt();
+        
+        
+        if (a == 0) {
+            System.out.println("This is not a quadratic equation.");
+            return; 
+        }
 
-}}
+        System.out.println("Enter the value of b:");
+        int b = sc.nextInt();
+        
+        System.out.println("Enter the value of c:");
+        int c = sc.nextInt();
+        
+        
+        int d = b * b - 4 * a * c;
+        
+        if (d == 0) {
+            
+            double r1 = -b / (2.0 * a);
+            System.out.println("The roots are real and equal: " + r1);
+        } 
+        else if (d > 0) {
+            
+            double r1 = (-b + Math.sqrt(d)) / (2 * a);
+            double r2 = (-b - Math.sqrt(d)) / (2 * a);
+            System.out.println("The roots are real and distinct: " + r1 + " and " + r2);
+        } 
+        else {
+            
+            double realPart = -b / (2.0 * a);
+            double imaginaryPart = Math.sqrt(-d) / (2.0 * a);
+            System.out.println("The roots are imaginary: " + realPart + " ± " + imaginaryPart + "i");
+        }
+    }
+}
