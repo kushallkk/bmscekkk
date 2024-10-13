2f321980446deff5b9a7271fd6aaeced0122c5c3
kushallkk <kushallk.cs23@bmsce.ac.in>
2024-10-14 01:57:05 +0530

    Update student.java

--- student.java
+++ student.java
@@ -1,67 +1,71 @@
 import java.util.Scanner;
-class studentdet{
-int marks[] = new int[3];
-String usn,name,grade;
-int credits = 12;
-int scgpa;
-Scanner sc = new Scanner(System.in);
 
-void getdetails(){
-usn=sc.next();
-name=sc.next();
-for(int i = 0;i<3;i++){
-marks[i]=sc.nextInt();
-}
-}
-void display(){
-System.out.println("usn: "+usn);
-System.out.println("name: "+name);
-for(int i = 0; i<3;i++){
-System.out.println("the marks of the three subjects are: "+marks[i]);
-}
-}
+class StudentDet {
+    int marks[] = new int[3];
+    String usn, name, grade;
+    int credits = 12;
+    double scgpa;
+    Scanner sc = new Scanner(System.in);
 
-void calcscgpa(){
-int sum=0;
-for(int i=0;i<3;i++){
-sum = marks[i] + sum;
-}
-scgpa = sum/credits;
-if(scgpa<100){
-grade = "S";
-}
-else if(scgpa<90){
-grade = "A+";
-}
-else if(scgpa<80){
-grade = "A";
-}
-else if(scgpa<70){
-grade = "B+";
-}
-else if(scgpa<60){
-grade = "B";}
-else{
-grade="F";
-}
-System.out.println("the scgpa and grade is "+scgpa+" and "+grade);
-}
-}
+    void getDetails() {
+        System.out.print("Enter USN and Name: ");
+        usn = sc.next();
+        name = sc.next();
+        System.out.println("Enter marks for 3 subjects:");
+        for (int i = 0; i < 3; i++) {
+            marks[i] = sc.nextInt();
+        }
+    }
 
-class student{
-public static void main(String args[]){
-studentdet s1[]=new studentdet[3];
-for(int j = 0; j<3 ; j++){
-s1[j] = new studentdet();
-}
-for(int i = 0 ; i<3;i++){
-System.out.println("enter the marks of student"+i+1);
-s1[i].getdetails();
+    void display() {
+        System.out.println("USN: " + usn);
+        System.out.println("Name: " + name);
+        System.out.println("Marks for the 3 subjects are:");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Subject " + (i + 1) + ": " + marks[i]);
+        }
+    }
+
+    void calcScgpa() {
+        int sum = 0;
+        for (int i = 0; i < 3; i++) {
+            sum += marks[i];
+        }
+        // Assuming 100 is the max marks per subject
+        scgpa = (sum / 3.0) * (credits / 30.0); // Adjusting based on the credits
+
+        if (scgpa >= 9) {
+            grade = "S";
+        } else if (scgpa >= 8) {
+            grade = "A+";
+        } else if (scgpa >= 7) {
+            grade = "A";
+        } else if (scgpa >= 6) {
+            grade = "B+";
+        } else if (scgpa >= 5) {
+            grade = "B";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("SCGPA: " + scgpa + " Grade: " + grade);
+    }
 }
 
-for(int j =0;j<3;j++){
-s1[j].display();
-s1[j].calcscgpa();
-}
-}
+class Student {
+    public static void main(String args[]) {
+        StudentDet s1[] = new StudentDet[3];
+        for (int j = 0; j < 3; j++) {
+            s1[j] = new StudentDet();
+        }
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Enter details for student " + (i + 1) + ":");
+            s1[i].getDetails();
+        }
+
+        for (int j = 0; j < 3; j++) {
+            s1[j].display();
+            s1[j].calcScgpa();
+        }
+    }
 }
